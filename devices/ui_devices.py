f142feabb0a95bbd8fee43fc025a60c3b3bc8d26
JuneSunAt7 <jprogram@bk.ru>
2024-03-29 17:14:06 +0300

    create finder

--- devices/ui_devices.py
+++ devices/ui_devices.py
@@ -1,36 +1,28 @@
 from colorama import Fore, init
-from devices.ui_devices import Device
+from devices.finder import finder
 
-class Terminal:
+class Device:
     def __init__(self):
         init()
         self.options = []
         self.__update_options()
 
-
     def __update_options(self):
-        self.options.append("Devices")
-        self.options.append("Messages")
-        self.options.append("Broker && topic settings")
-        self.options.append("Networking")
-        self.options.append("Security")
+        self.options.append("Find all devs")
+        self.options.append("Add device")
+        self.options.append("Show all devices")
+        self.options.append("Get info")
         self.options.append("Quit")
 
     def show_menu(self):
         for i, element in enumerate(self.options, start=1):
-            print(Fore.GREEN + f'[{i}] { Fore.MAGENTA + element}')
+            print(Fore.GREEN + f'[{i}] {Fore.MAGENTA + element}')
 
         operation = int(input(Fore.RESET + "Number of operation: "))
         self.__switch_operation(operation)
 
     def __switch_operation(self, selectedNum):
         if selectedNum == 1:
-            dev = Device()
-            dev.show_menu()
-
+            finder()
         else:
             print("Operation not implemented yet.")
-
-if __name__ == '__main__':
-    term = Terminal()
-    term.show_menu()
